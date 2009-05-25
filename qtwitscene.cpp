442de52d6c3d2c2a5eaa7bffa0fd8d667a256636
Toni Jovanoski <minimoog@.(none)>
2009-05-25 03:59:29 +0200

    Added cc license for graphic design.

--- qtwitscene.cpp
+++ qtwitscene.cpp
@@ -18,6 +18,15 @@
  * Contact e-mail: Antonie Jovanoski <minimoog77_at_gmail.com>
  */
 
+/*
+ * This work is licensed under the Creative Commons Attribution-Noncommercial-Share 
+ * Alike 3.0 Unported License. To view a copy of this license, 
+ * visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to 
+ * Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+ *
+ * Graphic design by Goce Mitevski 
+ */
+
 #include <QtDebug>
 #include <QScrollBar>
 #include <QGraphicsView>
